a77fa0f40bb38d274b5f3761fba3dacbaf1314d4
Rohan Rai <92577040+Rohanrai540@users.noreply.github.com>
2026-03-22 18:51:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your code here
+function majorityElement(arr) {
+    let map = {};
+
+    for (let num of arr) {
+        map[num] = (map[num] || 0) + 1;
+
+        if (map[num] > Math.floor(arr.length / 2)) {
+            return num;
+        }
+    }
+}
